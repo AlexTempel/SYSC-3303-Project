d07e86799add639c646593fb006d5a3d950cae6d
Jake <jakecalder@cmail.carleton.ca>
2024-01-30 11:51:30 -0500

    add setters/getters

--- src/Request.java
+++ src/Request.java
@@ -1,5 +1,27 @@
 public class Request {
-    private int time;
-    private int starting_floor;
-    private int ending_floor;
+    private int time; //hh:mm:ss.mmm
+    private final int starting_floor;
+    private final int ending_floor;
+
+    public Request(int time, int starting_floor, int ending_floor) {
+        this.time = time;
+        this.starting_floor = starting_floor;
+        this.ending_floor = ending_floor;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getStarting_floor() {
+        return starting_floor;
+    }
+
+    public int getEnding_floor() {
+        return ending_floor;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
 }
