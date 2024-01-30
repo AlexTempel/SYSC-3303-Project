2fc6f0aece5fe019f2e565d021688b8916ce4705
Darpyface <alex.dirk.tempel@gmail.com>
2024-01-30 10:44:35 -0500

    Added Runnable to Floor Subsystem

--- src/Floor.java
+++ src/Floor.java
@@ -1,7 +1,11 @@
-public class Floor {
+public class Floor implements Runnable{
     private final int floorNumber;
 
     Floor(int floor) {
         floorNumber = floor;
+    }
+
+    public void run() {
+        System.out.println("I am a Floor");
     }
 }
