fab75533ffbbe102b7259402cf89111df590224d
Darpyface <alex.dirk.tempel@gmail.com>
2024-01-30 10:41:36 -0500

    Start of the project

---
+++ src/Elevator.java
@@ -0,0 +1,8 @@
+public class Elevator implements Runnable{
+    Elevator() {
+
+    }
+    public void run() {
+        System.out.println("I am an Elevator");
+    }
+}
---
+++ src/Scheduler.java
@@ -0,0 +1,8 @@
+public class Scheduler implements Runnable{
+    Scheduler() {
+
+    }
+    public void run() {
+        System.out.println("I am the Scheduler");
+    }
+}
