8a153ab771e7b1a900f83b993861fb007665d111
Darpyface <alex.dirk.tempel@gmail.com>
2024-01-30 11:57:00 -0500

    Added some sutff for the FLoor Subsystem

--- src/FloorSubsystem.java
+++ src/FloorSubsystem.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class FloorSubsystem implements Runnable {
 
+    ArrayList<Floor> listOfFloors;
+    ArrayList<Request> listOfRequests;
+
+    FloorSubsystem(int numberOfFloors) {
+        listOfFloors = new ArrayList<>();
+        for (int i = 0; i < numberOfFloors; i++) {
+            listOfFloors.add(new Floor(i+1));
+        }
+        listOfRequests = readCSV("Input.csv");
+    }
+
+    private static ArrayList<Request> readCSV(String csvName) {
+        return new ArrayList<>();
+    }
     public void run() {
 
     }
