a2c791945778e6b747d04bc05d8c406d3f95b92b
nikisundefined <n.nemec21@gmail.com>
2024-02-02 23:22:26 -0500

    Finished implementation of csv reader and request arraylist creation

--- src/Request.java
+++ src/Request.java
@@ -1,35 +1,44 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
+import java.util.Date;
+
 public class Request {
-    private int time; //hh:mm:ss.mmm
-    private final int starting_floor;
-    private final int ending_floor;
+    private Date time;
+    //private int time; //hh:mm:ss.mmm
+    private final int floor;
+    private final int car_button;
+    private final String floor_button;
 
     private Boolean complete = false;
 
-    public Request(int time, int starting_floor, int ending_floor) {
+    public Request(Date time, int floor, String floor_button, int car_button) {
         this.time = time;
-        this.starting_floor = starting_floor;
-        this.ending_floor = ending_floor;
+        this.floor = floor;
+        this.floor_button = floor_button;
+        this.car_button = car_button;
     }
 
     public boolean getComplete() {
         return complete;
     }
+
     public void finish() {
         complete = true;
     }
-    public int getTime() {
+
+    public Date getTime() {
         return time;
     }
 
     public int getStarting_floor() {
-        return starting_floor;
+        return floor;
     }
 
     public int getEnding_floor() {
-        return ending_floor;
+        return car_button;
     }
 
-    public void setTime(int time) {
+    public void setTime(Date time) {
         this.time = time;
     }
 }
