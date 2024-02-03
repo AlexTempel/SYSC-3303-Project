4b427951e1e7f10472079dce8a3599e1a197c1ba
Jake <jakecalder@cmail.carleton.ca>
2024-02-03 18:29:53 -0500

    working with LocalTime

--- src/ElevatorTest.java
+++ src/ElevatorTest.java
@@ -9,7 +9,7 @@ class ElevatorTest {
     void test(){
 
         //Request testRequest = new Request(1,3,5);
-        Request testRequest = new Request(LocalTime.now(),3,5);
+        Request testRequest = new Request(LocalTime.now(),3,"Up", 5);
 
         Elevator testElevator = new Elevator(1,testRequest);
 
