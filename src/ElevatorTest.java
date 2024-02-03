c750b7fcb6a83b412148b90443fde434c64e4350
Jake <jakecalder@cmail.carleton.ca>
2024-02-03 18:27:51 -0500

    working with LocalTime

--- src/ElevatorTest.java
+++ src/ElevatorTest.java
@@ -1,10 +1,16 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+LocalTime
 class ElevatorTest {
     @Test
     void test(){
 
-        Request testRequest = new Request(1,3,5);
+        //Request testRequest = new Request(1,3,5);
+        Request testRequest = new Request(LocalTime.now(),3,5);
+
         Elevator testElevator = new Elevator(1,testRequest);
 
     }
