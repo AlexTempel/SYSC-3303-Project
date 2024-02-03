03771a877e723824c19478c9a0e0b0e00748cd69
Darpyface <61155139+Darpyface@users.noreply.github.com>
2024-02-03 18:20:41 -0500

    Started to implement Main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        Request floorbuffer = null;
+        Request elevatorbuffer = null;
+
         FloorSubsystem floorSubsystem = new FloorSubsystem();
+        Scheduler scheduler = new Scheduler();
+        Elevator elevator = new Elevator();
     }
 }
