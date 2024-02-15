61b34477e1a94e7c84db8b59133f9e67023ad83f
Darpyface <61155139+Darpyface@users.noreply.github.com>
2024-02-14 20:12:58 -0500

    Iteration 1 Submission

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Request[] floorbuffer = new Request[1];
+        Request[] floorbuffer = new Request[1]; //Buffer
         Request[] elevatorbuffer = new Request[1];
 
         FloorSubsystem floorSubsystem = new FloorSubsystem(22, floorbuffer);
